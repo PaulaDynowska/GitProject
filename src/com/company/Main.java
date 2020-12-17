49151a3f2b007361d0f643da302b4f1fcd07301e
PDynowska <paulina.dynowska_w>
2020-12-17 10:57:11 +0100

    zombie appears

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,11 +3,14 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello! ");
-        System.out.println("This is kinda magic....");
-        System.out.println("bla bla");
-        System.out.println("Another one bites the dust....");
-        System.out.println("turururururur");
-        System.out.println("Another one here");
+//        System.out.println("Hello! ");
+//        System.out.println("This is kinda magic....");
+//        System.out.println("bla bla");
+//        System.out.println("Another one bites the dust....");
+//        System.out.println("turururururur");
+//        System.out.println("Another one here");
+
+        Zombie z1 = new Zombie("Ugly Bob", "Crawler", 55);
+        System.out.println(z1.toString());
     }
 }
